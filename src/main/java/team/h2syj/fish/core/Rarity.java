52d211e30ea56f9fad64bc764f65f97eff77a9ea
胡杰 <h2syj.zzzz@gmail.com>
2024-03-25 17:24:10 +0800

    添加稀有等级注解

---
+++ src/main/java/team/h2syj/fish/core/Rarity.java
@@ -0,0 +1,20 @@
+package team.h2syj.fish.core;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target({ElementType.TYPE})
+@Retention(RetentionPolicy.RUNTIME)
+public @interface Rarity {
+
+    /**
+     * 物品稀有等级
+     * 暂定 1-3 级
+     *
+     * @return 物品稀有等级
+     */
+    int value();
+
+}
